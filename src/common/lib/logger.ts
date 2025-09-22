93b98c614b1fb2b5ee176b886dcc5f6b84dde18b
Aoou <wynpay@example.com>
2025-09-22 15:48:48 +0800

    update

--- src/common/lib/logger.ts
+++ src/common/lib/logger.ts
@@ -1,29 +1,19 @@
 import winston from "winston"
-
-const timezoned = () => {
-    return new Date().toLocaleString("zh-CN", {
-        timeZone: "Asia/Shanghai",
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: false,
-    }) //.replace(/\//g, '-').replace(/,/g, '')
-}
+import { $dayjs } from "./utils"
 
 export const logger = winston.createLogger({
     level: process.env.LOG_LEVEL || "info",
     format: winston.format.combine(
         // 时间日期格式
         winston.format.timestamp({
-            format: timezoned,
+            format: () => $dayjs().utcOffset(8).format("YYYY-MM-DDTHH:mm:ssZ"),
         }),
+        winston.format.json(),
+        winston.format.errors({ stack: true }),
         // 打印格式
-        winston.format.printf(({ timestamp, level, message }: any) => {
-            return `${timestamp} ${level}: ${message}`
-        }),
+        // winston.format.printf(({ timestamp, level, message }: any) => {
+        //     return `${timestamp} ${level}: ${message}`
+        // }),
     ),
     transports: [new winston.transports.Console()],
 })
--- src/crons/test.ts
+++ src/crons/test.ts
@@ -2,6 +2,7 @@ import { logger } from "@/common/lib/logger"
 
 export const handler = async (event: any) => {
     try {
+        // 
         logger.info(`开始测试任务, 测试数据库连接:${process.env.DATABASE_URL_REPORTING}`)
     } catch (error) {
         logger.error("测试任务执行错误")
